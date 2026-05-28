c1f02f5f828e3535747c0deedae1a5ba73d8e099
lhn_nryn <dubmate2244@gmail.com>
2026-05-28 14:30:49 +0545

    add route for login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { routes } from "./constants/routes";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Navigate to={routes.register} replace />} />
+            <Route
+                path="/"
+                element={<Navigate to={routes.register} replace />}
+            />
             <Route path={routes.register} element={<RegisterPage />} />
+            <Route path={routes.login} element={<LoginPage />} />
         </Routes>
     );
 }
