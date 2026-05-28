74025ca7022921b5e1115fbc5875a0a90f04650d
lhn_nryn <dubmate2244@gmail.com>
2026-05-28 14:29:37 +0545

    add post api to login user

--- frontend/src/services/auth.api.ts
+++ frontend/src/services/auth.api.ts
@@ -1,11 +1,30 @@
 import { api } from "../lib/api";
 import type { ApiResponse } from "../types/api";
-import type { RegisterRequest, RegisterResponse } from "../types/auth";
+import type {
+    RegisterRequest,
+    RegisterResponse,
+    LoginRequest,
+    LoginResponse,
+} from "../types/auth";
 
 export const registerUser = async (
     payload: RegisterRequest,
 ): Promise<ApiResponse<RegisterResponse>> => {
-    const response = await api.post<ApiResponse<RegisterResponse>>("/auth/register", payload);
+    const response = await api.post<ApiResponse<RegisterResponse>>(
+        "/auth/register",
+        payload,
+    );
+
+    return response.data;
+};
+
+export const loginUser = async (
+    payload: LoginRequest,
+): Promise<ApiResponse<LoginResponse>> => {
+    const response = await api.post<ApiResponse<LoginResponse>>(
+        "/auth/login",
+        payload,
+    );
 
     return response.data;
 };
