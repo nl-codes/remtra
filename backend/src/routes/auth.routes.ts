5d01d0cc3470efd684fd5e5415723a1e4113324a
lhn_nryn <dubmate2244@gmail.com>
2026-05-28 11:08:51 +0545

    add route for login with validation using loginSchema

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller.js";
+import { register, login } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/zod.middleware.js";
-import { registerSchema } from "../schemas/auth.schema.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
 router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
 
 export default router;
