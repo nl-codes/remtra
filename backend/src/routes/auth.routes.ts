2d6a93d56b042f851a7f24772e1946bbebdc4daa
lhn_nryn <dubmate2244@gmail.com>
2026-06-08 11:13:22 +0545

    feat(routes): add logout route with authentication middleware

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -5,8 +5,10 @@ import {
     forgotPassword,
     verifyResetPasswordToken,
     resetPassword,
+    logout,
 } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/zod.middleware.js";
+import { requireAuth } from "../middlewares/auth.middleware.js";
 import {
     registerSchema,
     loginSchema,
@@ -17,9 +19,9 @@ import {
 
 const router = Router();
 
+// Public Routes (no Authorization required)
 router.post("/register", validate(registerSchema), register);
 router.post("/login", validate(loginSchema), login);
-
 router.post("/forgot-password", validate(forgotPasswordSchema), forgotPassword);
 router.get(
     "/validate-reset-password-token/:token",
@@ -28,4 +30,6 @@ router.get(
 );
 router.post("/reset-password", validate(resetPasswordSchema), resetPassword);
 
+// Protected Routes (Authorization required)
+router.post("/logout", requireAuth, logout);
 export default router;
