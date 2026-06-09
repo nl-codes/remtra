d62735eee46e73270544c27c8e9f3f0d9b6c726f
lhn_nryn <dubmate2244@gmail.com>
2026-06-09 13:17:31 +0545

    feat(routes): add routes for retrieving profiles of owm and by using userId

--- backend/src/routes/profile.routes.ts
+++ backend/src/routes/profile.routes.ts
@@ -2,11 +2,25 @@ import { Router } from "express";
 
 import { validate } from "../middlewares/zod.middleware.js";
 import { requireAuth } from "../middlewares/auth.middleware.js";
-import { registerProfileSchema } from "../schemas/profile.schema.js";
-import { registerProfile } from "../controllers/profile.controller.js";
+import {
+    getProfileSchema,
+    registerProfileSchema,
+} from "../schemas/profile.schema.js";
+import {
+    getMyProfile,
+    getProfileByUserId,
+    registerProfile,
+} from "../controllers/profile.controller.js";
 
 const router = Router();
 
 router.post("/", requireAuth, validate(registerProfileSchema), registerProfile);
+router.get("/", requireAuth, getMyProfile);
+router.get(
+    "/user/:userId",
+    requireAuth,
+    validate(getProfileSchema),
+    getProfileByUserId,
+);
 
 export default router;
